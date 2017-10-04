a50177b7e29ee14b236f78fd4f89cc038ff6b768
Rosie Armstrong <rosemary_a@live.co.uk>
2017-10-04 14:31:41 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Welcome to React</h1>
+        <p>Start working from <code>/src/components/App.js</code></p>
+      </div>
+    );
+  }
+}
+
+export default App;
+
